5ec91ed41bd18cc496296d7727817e209a84de06
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-12 10:34:11 -0300

    Criando classe People com atributos e metodos
    Aula 11 aprendendo Herança

---
+++ Aula10/Class/People.php
@@ -0,0 +1,39 @@
+<?php
+
+class People {
+
+    private $name;
+    private $age;
+    private $sex;
+
+    public function birth() {
+        $this->age++;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($n) {
+        $this->name = $n;
+    }
+    
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function setAge($a) {
+        $this->age = $a;
+    }
+
+    public function getSex() {
+        return $this->sex;
+    }
+
+    public function setSex($s) {
+        $this->sex = $s;
+    }
+
+}
+
+?>
