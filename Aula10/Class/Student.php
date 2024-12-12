e34e5fd822ce2ee2a50a4d3ba7ad14d37b1bbf50
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2024-12-12 10:45:18 -0300

    Adicionando classe Student

---
+++ Aula10/Class/Student.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once './Class/People.php';
+
+class Student extends People {
+    
+    private $registration;
+    private $course;
+
+    public function cancelRegistrtion() {
+        echo '<p>Matricuka será cancelada</p>';
+    }
+
+    public function getRegistration() {
+        return $this->registration;
+    }
+
+    public function setRegistration($r) {
+        $this->registration = $r;
+    } 
+
+    public function getCourse() {
+        return $this->course;
+    }
+
+    public function setCourse($c) {
+        $this->course = $c;
+    }
+}
+
+?>
